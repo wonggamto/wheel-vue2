c2cdc194a6ce2c13a348cb488c4755739cb30fc5
Tom <wonggamto@163.com>
2020-08-24 22:44:30 +0800

    完成翻页按钮

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
-import Button from '../src/button.vue'
-import Icon from '../src/icon'
-
+import Button from './button.vue'
+import Icon from './icon'
+import ButtonGroup from './button-group.vue'
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
+Vue.component('g-button-group',ButtonGroup)
 new Vue({
   el: '#app',
   data: {
