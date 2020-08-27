2ea925f35f40d79db1eebe80d606963a9561d912
Tom <wonggamto@163.com>
2020-08-27 16:58:53 +0800

    添加 input.vue 及其 样式

--- src/app.js
+++ src/app.js
@@ -2,9 +2,11 @@ import Vue from 'vue'
 import Button from './button.vue'
 import Icon from './icon'
 import ButtonGroup from './button-group.vue'
+import Input from './input'
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
 Vue.component('g-button-group',ButtonGroup)
+Vue.component('g-input',Input)
 new Vue({
   el: '#app',
   data: {
@@ -13,65 +15,3 @@ new Vue({
     loading3: false
   }
 })
-import chai from 'chai'
-import spies from 'chai-spies'
-chai.use(spies)
-const expect = chai.expect
-// 单元测试
-{//icon测试
-  const Constructor = Vue.extend(Button)
-  const vm = new Constructor({
-    propsData:{
-      icon:'setting'
-    }
-  })
-  vm.$mount()
-  let useElement = vm.$el.querySelector('use')
-  expect(useElement.getAttribute('xlink:href')).to.eq('#icon-setting')
-  vm.$el.remove()
-  vm.$destroy()
-}
-{//loading测试
-  const Constructor = Vue.extend(Button)
-  const vm = new Constructor({
-    propsData:{
-      icon:'setting',
-      loading:true
-    }
-  })
-  vm.$mount()
-  let useElement = vm.$el.querySelector('use')
-  expect(useElement.getAttribute('xlink:href')).to.eq('#icon-loading')
-  vm.$el.remove()
-  vm.$destroy()
-}
-{//order测试
-  const div = document.createElement('div')
-  document.body.appendChild(div)
-  const Constructor = Vue.extend(Button)
-  const vm = new Constructor({
-    propsData:{
-      icon:'setting',
-      iconPosition:'right'
-    }
-  })
-  vm.$mount(div)
-  let svg = vm.$el.querySelector('svg')
-  let order = window.getComputedStyle(svg).order
-  expect(order).to.eq('2')
-  vm.$el.remove()
-  vm.$destroy()
-}{//click测试
-  const Constructor = Vue.extend(Button)
-  const vm = new Constructor({
-    propsData:{
-      icon:'setting',
-    }
-  })
-  vm.$mount()
-  let spy = chai.spy(function (){})
-  vm.$on('click',spy)
-  let button = vm.$el
-  button.click()
-  expect(spy).to.have.been.called()
-}
---
+++ src/input.vue
@@ -0,0 +1,79 @@
+<template>
+    <div class="wrapper" :class="{'error':error}">
+        <input type="text" :value="value" :disabled="disable" :readonly="readonly"/>
+        <template v-if="error">
+            <icon name="error" class="icon-error"></icon>
+            <span class="error-message">{{error}}</span>
+        </template>
+    </div>
+</template>
+<script>
+  import Icon from "./icon"
+
+  export default {
+    components: {'icon': Icon},
+    name: 'GuluInput',
+    props: {
+      value: {
+        type: String
+      },
+      disable: {
+        type: Boolean,
+        default: false
+      },
+      readonly: {
+        type: Boolean,
+        default: false
+      },
+      error: {
+        type: String
+      }
+    }
+  }
+</script>
+<style lang="scss" scoped>
+    $height: 32px;
+    $border-color: #999;
+    $border-color-hover: #666;
+    $border-radius: 4px;
+    $font-size: 12px;
+    $box-shadow-color: rgba(0, 0, 0, 0.5);
+    $red: #F1453D;
+    .wrapper {
+        font-size: $font-size;
+        display: inline-flex;
+        align-items: center;
+        > :not(:last-child){margin-right: 0.5em;}
+        > input {
+            height: $height;
+            border: 1px solid $border-color;
+            border-radius: $border-radius;
+            padding: 0 8px;
+            font-size: inherit;
+            &:hover {
+                border-color: $border-color-hover;
+            }
+            &:focus {
+                box-shadow: inset 0 1px 3px $box-shadow-color;
+                outline: none;
+            }
+            &[disabled], [readonly] {
+                border-color: #aaa;
+                color: #aaa;
+                cursor: not-allowed;
+            }
+        }
+        &.error {
+            > input {
+                border-color: $red;
+            }
+        }
+        .icon-error {
+            fill: $red;
+        }
+        .error-message {
+            color: $red;
+        }
+
+    }
+</style>
