484174beaeaf34b0686dfc360c91106e472a5bbc
Tom <wonggamto@163.com>
2020-08-28 15:23:38 +0800

    添加 input 组件的测试用例

--- src/input.vue
+++ src/input.vue
@@ -1,6 +1,11 @@
 <template>
     <div class="wrapper" :class="{'error':error}">
-        <input type="text" :value="value" :disabled="disable" :readonly="readonly"/>
+        <input type="text" :value="value" :disabled="disable" :readonly="readonly"
+        @change="$emit('change',$event)"
+        @input="$emit('input',$event)"
+        @focus="$emit('focus',$event)"
+        @blur="$emit('blur',$event)"
+        />
         <template v-if="error">
             <icon name="error" class="icon-error"></icon>
             <span class="error-message">{{error}}</span>
---
+++ test/input.test.js
@@ -0,0 +1,79 @@
+const expect = chai.expect;
+import Vue from 'vue'
+import Input from '../src/input'
+
+Vue.config.productionTip = false
+Vue.config.devtools = false
+
+describe('Input', () => {
+  it('存在.', () => {
+    expect(Input).to.be.exist
+  })
+  describe('props', () => {
+    const Constructor = Vue.extend(Input)
+    let vm
+    afterEach(() => {
+      vm.$destroy()
+    })
+    it('接收 value', () => {
+      vm = new Constructor({
+        propsData: {
+          value: '1234'
+        }
+      }).$mount()
+      const inputElement = vm.$el.querySelector('input')
+      expect(inputElement.value).to.equal('1234')
+      vm.$destroy()
+    })
+    it('接收 disable', () => {
+      vm = new Constructor({
+        propsData: {
+          disable: true
+        }
+      }).$mount()
+      const inputElement = vm.$el.querySelector('input')
+      expect(inputElement.disabled).to.equal(true)
+      vm.$destroy()
+    })
+    it('接收 只读', () => {
+      vm = new Constructor({
+        propsData: {
+          readonly: true
+        }
+      }).$mount()
+      const inputElement = vm.$el.querySelector('input')
+      expect(inputElement.readOnly).to.equal(true)
+      vm.$destroy()
+    })
+    it('接收 error', () => {
+      vm = new Constructor({
+        propsData: {
+          error: "错误信息"
+        }
+      }).$mount()
+      const useElement = vm.$el.querySelector('use')
+      expect(useElement.getAttribute('xlink:href')).to.equal('#icon-error')
+      const errorMessage = vm.$el.querySelector('.error-message')
+      expect(errorMessage.innerText).to.equal('错误信息')
+      vm.$destroy()
+    })
+  })
+  describe('事件', () => {
+    const Constructor = Vue.extend(Input)
+    let vm
+    afterEach(() => {
+      vm.$destroy()
+    })
+    it('支持 change/input/focus/blur事件', () => {
+      ['change', 'input', 'focus', 'blur'].forEach((eventName) => {
+        vm = new Constructor({}).$mount()
+        const callback = sinon.fake()
+        vm.$on(eventName, callback)
+        let event = new Event(eventName)
+        let inputElement = vm.$el.querySelector('input')
+        inputElement.dispatchEvent(event)
+        expect(callback).to.have.been.calledWith(event)
+      })
+    })
+  })
+})
