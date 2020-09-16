4d89b74f4d1c9f1d3f0b7d4700da19244258017b
Tom <wonggamto@163.com>
2020-09-16 23:40:38 +0800

    button.vue 的css变量变更为 scss变量

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  title: '极 UI',
+  description: '一个好用的UI框架',
+  themeConfig: {
+    sidebar: [
+      {
+        title:'入门',
+        children: [
+          '/install/',
+          '/get_started/',]
+      },
+
+      {
+        title:'组件',
+        children:['/components/button']
+      }
+    ]
+  }
+}
