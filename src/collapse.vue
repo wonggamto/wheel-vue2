a7d5de12d48a7c88d9764b442ce62d396af0a6c0
Tom <wonggamto@163.com>
2020-09-16 18:01:36 +0800

    添加 single 选项

--- src/collapse.vue
+++ src/collapse.vue
@@ -3,18 +3,36 @@
         <slot></slot>
     </div>
 </template>
-
 <script>
-     export default {
-       name:'GuluCollapse'
-     }
-</script>
+  import Vue from 'vue'
 
+  export default {
+    name: 'GuluCollapse',
+    props: {
+      single:{
+        type:Boolean,
+        default:false
+      }
+    },
+    data() {
+      return {
+        eventBus: new Vue
+      }
+    },
+    provide() {
+        if(this.single){
+          return {
+            eventBus: this.eventBus
+          }
+      }
+    }
+  }
+</script>
 <style lang="scss" scoped>
-    $grey:#ddd;
-    $border-radius:4px;
-    .collapse{
-        border:1px solid $grey;
+    $grey: #ddd;
+    $border-radius: 4px;
+    .collapse {
+        border: 1px solid $grey;
         border-radius: $border-radius;
     }
 </style>
