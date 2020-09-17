ccd1d1401098dd40197df7384dfac0413f053801
Tom <wonggamto@163.com>
2020-09-17 10:57:06 +0800

    创建官网

--- docs/.vuepress/components/button-demos.vue
+++ docs/.vuepress/components/button-demos.vue
@@ -1,17 +1,30 @@
 <template>
     <div>
         <g-button>按钮1</g-button>
-
         <g-button icon="setting">按钮2</g-button>
-        <g-button :loading="true">按钮3</g-button>
+        <g-button :loading="true" icon-position="right">按钮3</g-button>
         <g-button disabled>按钮4</g-button>
+        <pre>
+            <code>{{content}}</code>
+        </pre>
     </div>
 </template>
 <script>
-    import Button from '../../../src/button'
-    export default {
-      components:{
-        'g-button':Button
+  import Button from '../../../src/button'
+
+  export default {
+    components: {
+      'g-button': Button
+    },
+    data() {
+      return {
+        content: `
+        <g-button>按钮1</g-button>
+        <g-button icon="setting">按钮2</g-button>
+        <g-button :loading="true" icon-position="right">按钮3</g-button>
+        <g-button disabled>按钮4</g-button>
+        `
       }
     }
+  }
 </script>
